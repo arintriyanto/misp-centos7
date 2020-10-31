1b895cacf15d281f74f3e7ae7f2192f9cfb0dc7a
Arin Triyanto <arintriyanto@gmail.com>
2020-11-01 02:06:12 +0700

    update script misp-dashboard

--- misp.dashboard.sh
+++ misp.dashboard.sh
@@ -55,22 +55,22 @@ checkFlavour () {
 
 }
 
+# <snippet-begin 4_misp-dashboardRHEL.sh>
 # Main MISP Dashboard install function
-mispDashboardRHEL () {
-  yum install wget screen -y
-  mkdir /var/www/misp-dashboard
-  chown $WWW_USER:$WWW_USER /var/www/misp-dashboard
+mispDashboard () {
+  sudo yum install wget screen -y
+  sudo mkdir /var/www/misp-dashboard
+  sudo chown $WWW_USER:$WWW_USER /var/www/misp-dashboard
   $SUDO_WWW git clone https://github.com/MISP/misp-dashboard.git /var/www/misp-dashboard
   cd /var/www/misp-dashboard
-  sed -i -E 's/apt/#apt/' install_dependencies.sh
-  sed -i -E 's/rhel/centos/' install_dependencies.sh
-  sed -i -E 's/virtualenv -p python3 DASHENV/\/usr\/bin\/scl enable rh-python36 \"virtualenv -p python3 DASHENV\"/' install_dependencies.sh
-  /var/www/misp-dashboard/install_dependencies.sh
-  sed -i "s/^host\ =\ localhost/host\ =\ 0.0.0.0/g" /var/www/misp-dashboard/config/config.cfg
-  sed -i '/Listen 80/a Listen 0.0.0.0:8001' /etc/httpd/conf/httpd.conf
-  yum install rh-python36-mod_wsgi -y
-  cp -f /opt/rh/httpd24/root/usr/lib64/httpd/modules/mod_rh-python36-wsgi.so /etc/httpd/modules/
-  cp -f /opt/rh/httpd24/root/etc/httpd/conf.modules.d/10-rh-python36-wsgi.conf /etc/httpd/conf.modules.d/
+  sudo sed -i -E 's/sudo apt/#sudo apt/' install_dependencies.sh
+  sudo sed -i -E 's/virtualenv -p python3 DASHENV/\/usr\/bin\/scl enable rh-python36 \"virtualenv -p python3 DASHENV\"/' install_dependencies.sh
+  sudo -H /var/www/misp-dashboard/install_dependencies.sh
+  sudo sed -i "s/^host\ =\ localhost/host\ =\ 0.0.0.0/g" /var/www/misp-dashboard/config/config.cfg
+  sudo sed -i '/Listen 80/a Listen 0.0.0.0:8001' /etc/httpd/conf/httpd.conf
+  sudo yum install rh-python36-mod_wsgi -y
+  sudo cp /opt/rh/httpd24/root/usr/lib64/httpd/modules/mod_rh-python36-wsgi.so /etc/httpd/modules/
+  sudo cp /opt/rh/httpd24/root/etc/httpd/conf.modules.d/10-rh-python36-wsgi.conf /etc/httpd/conf.modules.d/
 
   echo "<VirtualHost *:8001>
       ServerAdmin admin@misp.local
@@ -108,13 +108,15 @@ mispDashboardRHEL () {
       ErrorLog /var/log/httpd/misp-dashboard.local_error.log
       CustomLog /var/log/httpd/misp-dashboard.local_access.log combined
       ServerSignature Off
-  </VirtualHost>" | tee /etc/httpd/conf.d/misp-dashboard.conf
+  </VirtualHost>" | sudo tee /etc/httpd/conf.d/misp-dashboard.conf
 
-  semanage port -a -t http_port_t -p tcp 8001
-  systemctl restart httpd.service
+  sudo semanage port -a -t http_port_t -p tcp 8001
+  sudo systemctl restart httpd.service 
+  sudo firewall-cmd --zone=public --add-port=8001/tcp --permanent
+  sudo firewall-cmd --reload
 
   # Add misp-dashboard to rc.local to start on boot.
-  sed -i -e '$i \-u apache bash /var/www/misp-dashboard/start_all.sh > /tmp/misp-dashboard_rc.local.log\n' /etc/rc.local
+  sudo sed -i -e '$i \sudo -u apache bash /var/www/misp-dashboard/start_all.sh > /tmp/misp-dashboard_rc.local.log\n' /etc/rc.local
 
   # Enable ZeroMQ for misp-dashboard
   $SUDO_WWW $RUN_PHP -- $CAKE Admin setSetting "Plugin.ZeroMQ_enable" true
@@ -134,8 +136,7 @@ mispDashboardRHEL () {
   $SUDO_WWW $RUN_PHP -- $CAKE Admin setSetting "Plugin.ZeroMQ_tag_notifications_enable" false
   $SUDO_WWW $RUN_PHP -- $CAKE Admin setSetting "Plugin.ZeroMQ_audit_notifications_enable" false
 }
-
-
+# <snippet-end 4_misp-dashboardRHEL.sh>
 
 echo "Checking Linux distribution and flavour..."
 checkFlavour
